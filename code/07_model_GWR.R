52ee3fec148c39113139792d65b577a768cc5820
Jakob Gutschlhofer <h1550545@wigeo0.wu-wien.ac.at>
2020-11-29 19:17:53 +0100

    Add basic structure for main.R, add code files

---
+++ code/07_model_GWR.R
@@ -0,0 +1,10 @@
+# Libraries
+
+# prepare X and y
+
+# write formula
+
+# find best neighbour specification
+
+# for n_neighbours in 1:n, do
+#     make W for each based on n_neighbours, run GWR
---
+++ code/main.R
@@ -0,0 +1,28 @@
+# Load libraries
+library(tidyverse)
+library(eurostat)
+
+# Download data ----------------------------------------------------------------
+# we will use NUTS3, we add the shapefile directly for all non-EUROSTAT stuff
+# after downloading, the data will be pre-processed to have a common standard
+# in particular, a long tibble/data.frame with column $indicator
+# variable names are lower case letters, the actual value is saved in $value
+
+# shape_nuts3 is our base-shapefile
+
+source("01_download_EDGAR.R") # output: data_edgar
+# output: data_eurostat
+# output: data_temperature
+
+# Combine data -----------------------------------------------------------------
+
+# output: data
+
+# Provide and visualise input data ---------------------------------------------
+
+# Run OLS ----------------------------------------------------------------------
+
+# Prepare and run GWR ----------------------------------------------------------
+
+# Extensions  ------------------------------------------------------------------
+
