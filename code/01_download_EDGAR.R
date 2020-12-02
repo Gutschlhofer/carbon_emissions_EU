96caf7ffef7c2b572b93adc00f37bb9f6b33e3a1
Benjamin Gerhard Müller <h1551800@wigeo0.wu-wien.ac.at>
2020-12-02 18:23:56 +0100

    Added code to download and munge EDGAR data

--- code/01_download_EDGAR.R
+++ code/01_download_EDGAR.R
@@ -1,2 +1,42 @@
-# .xls Timeseries in a .zip folder: 
-# https://edgar.jrc.ec.europa.eu/download.php?edgar_dst=188376
+## this file downloads the edgar data-------------------------------------------
+# we decided to use "CO2_excl_short-cycle_org_C"
+# we still need to discuss what excl short cycle means
+
+
+if(! file.exists("./input/data_edgar.rds")) {
+  
+  dir.create("input/EDGAR")
+  
+  download.file("https://edgar.jrc.ec.europa.eu/download.php?edgar_dst=187464",
+                "input/EDGAR/CO2_excl_short-cycle_org_C")
+  
+  unzip("input/EDGAR/CO2_excl_short-cycle_org_C", exdir = "./input/EDGAR")
+  
+  fs <- list.files("input/EDGAR", pattern = ".nc$", full.names = T)
+  
+  # only from 2000-2018
+  # fs <- fs[31:49]
+  
+  registerDoMC(49)
+  
+  shape_nuts3 <- getShapefile()
+  data_edgar <- data.frame()
+  data_edgar <- foreach(f = fs) %dopar% {
+    
+    b <- brick(f)
+    p <- b$layer 
+    ep <- raster::extract(p, shape_nuts3, method = "simple", fun = mean, na.rm = TRUE, df = TRUE, sp = TRUE)
+    
+    d <- ep@data %>%
+      select(nuts3_id, value = layer) %>%
+      mutate(year = as.numeric(substr(f, 44, 47)), indicator = c("edgar")) %>%
+      select(year, nuts3_id, value, indicator)
+    
+    return(d)
+  }
+  
+  data_edgar <- do.call(rbind, data_edgar)
+  saveRDS(data_edgar, "input/data_edgar.rds")
+  
+}
+
