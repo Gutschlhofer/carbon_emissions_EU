bcc4bb349656245f03b8b65fa2bab0f7ad1bcb7f
Benjamin Gerhard Müller <h1551800@wigeo0.wu-wien.ac.at>
2020-12-10 19:34:55 +0100

    Download EDGAR with updated shape and checked CRS

--- code/01_download_EDGAR.R
+++ code/01_download_EDGAR.R
@@ -1,9 +1,9 @@
 ## this file downloads the edgar data-------------------------------------------
 # we decided to use "CO2_excl_short-cycle_org_C"
-# we still need to discuss what excl short cycle means
-
 
 if(! file.exists("./input/data_edgar.rds")) {
+  
+  source("./code/00_libraries_functions.R")
   
   dir.create("input/EDGAR")
   
@@ -14,13 +14,17 @@ if(! file.exists("./input/data_edgar.rds")) {
   
   fs <- list.files("input/EDGAR", pattern = ".nc$", full.names = T)
   
-  # only from 2000-2018
-  # fs <- fs[31:49]
-  
-  registerDoMC(49)
+  # only for 2000-2018
+  fs <- fs[31:49]
   
   shape_nuts3 <- getShapefile()
+  #check if .nc files have the same projection as shapefile
+  crs(shape_nuts3)
+  crs(brick(fs[1])) # looks good
+  
   data_edgar <- data.frame()
+  
+  registerDoMC(20)
   data_edgar <- foreach(f = fs) %dopar% {
     
     b <- brick(f)
@@ -28,9 +32,9 @@ if(! file.exists("./input/data_edgar.rds")) {
     ep <- raster::extract(p, shape_nuts3, method = "simple", fun = mean, na.rm = TRUE, df = TRUE, sp = TRUE)
     
     d <- ep@data %>%
-      select(nuts3_id, value = layer) %>%
+      dplyr::select(nuts3_id, value = layer) %>%
       mutate(year = as.numeric(substr(f, 44, 47)), indicator = c("edgar")) %>%
-      select(year, nuts3_id, value, indicator)
+      dplyr::select(year, nuts3_id, value, indicator)
     
     return(d)
   }
