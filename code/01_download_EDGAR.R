2446c7222f6e4d0968b528ec5fbf2ce74f8247ea
Jakob Gutschlhofer <h1550545@wigeo0.wu-wien.ac.at>
2020-12-27 19:00:40 +0100

    Add exploratory code for .nc files #1

--- code/01_download_EDGAR.R
+++ code/01_download_EDGAR.R
@@ -18,9 +18,33 @@ if(! file.exists("./input/data_edgar.rds")) {
   fs <- fs[31:49]
   
   shape_nuts3 <- getShapefile()
-  #check if .nc files have the same projection as shapefile
-  crs(shape_nuts3)
-  crs(brick(fs[1])) # looks good
+  # check if .nc files have the same projection as shapefile
+  if(!compareCRS(shape_nuts3, brick(fs[1]))) print("Warning: shape and nc file have different CRS!")
+  
+  x <- brick(fs[19])$layer
+  raster::spplot(brick(fs[1])$layer)
+  plot(brick(fs[1])$layer)#, xlim=c(-9.507305e-10, 360),ylim=c(-90, 90))
+  
+  # r <- raster("nc/oisst-sst.nc")
+  library(leaflet)
+  pal <- colorNumeric(c("#0C2C84", "#41B6C4", "#FFFFCC"), values(x),
+                      na.color = "transparent")
+  
+  leaflet() %>% addTiles() %>%
+    addRasterImage(x, colors = pal, opacity = 0.8) %>%
+    addLegend(pal = pal, values = values(x),
+              title = "EDGAR stuffz")
+  # issue: data only from 0-meridian onwards
+  # therefore we lose UK, IE, parts of FR and ES, PT
+  
+  library(raster)
+  library(fasterize)
+  shp_raster <- st_transform(shp, old_crs)
+  shp_raster$random <- runif(nrow(shp_raster))
+  r <- raster(ncol=600, nrow=600)
+  ras <- fasterize(shp_raster, r, field = shp$random, fun = "sum")
+  plot(ras, xlim=c(-30,50),ylim=c(20,80))
+  
   
   data_edgar <- data.frame()
   
