86948aa57ff533589f1c3e4ff3ec59bf20ba9a80
Benjamin Gerhard Müller <h1551800@wigeo0.wu-wien.ac.at>
2020-12-29 17:59:00 +0100

    Use txt files instead of nc files for EDGAR data

--- code/01_download_EDGAR.R
+++ code/01_download_EDGAR.R
@@ -7,43 +7,24 @@ if(! file.exists("./input/data_edgar.rds")) {
   
   dir.create("input/EDGAR")
   
-  download.file("https://edgar.jrc.ec.europa.eu/download.php?edgar_dst=187464",
+  download.file("https://edgar.jrc.ec.europa.eu/download.php?edgar_dst=187465",
                 "input/EDGAR/CO2_excl_short-cycle_org_C")
   
   unzip("input/EDGAR/CO2_excl_short-cycle_org_C", exdir = "./input/EDGAR")
   
-  fs <- list.files("input/EDGAR", pattern = ".nc$", full.names = T)
+  fs <- list.files("input/EDGAR", pattern = ".txt$", full.names = T)
   
   # only for 2000-2018
   fs <- fs[31:49]
   
   shape_nuts3 <- getShapefile()
+
+  r <- read.delim(file = fs[1], header = T, sep = ";", dec = ".", skip = 2) %>%
+    dplyr::select(lon, lat, value = starts_with("emission")) %>%
+    rasterFromXYZ(crs = "+proj=longlat +datum=WGS84") #not sure about the datum but projection should be correct
+  
   # check if .nc files have the same projection as shapefile
-  if(!compareCRS(shape_nuts3, brick(fs[1]))) print("Warning: shape and nc file have different CRS!")
-  
-  x <- brick(fs[19])$layer
-  raster::spplot(brick(fs[1])$layer)
-  plot(brick(fs[1])$layer)#, xlim=c(-9.507305e-10, 360),ylim=c(-90, 90))
-  
-  # r <- raster("nc/oisst-sst.nc")
-  library(leaflet)
-  pal <- colorNumeric(c("#0C2C84", "#41B6C4", "#FFFFCC"), values(x),
-                      na.color = "transparent")
-  
-  leaflet() %>% addTiles() %>%
-    addRasterImage(x, colors = pal, opacity = 0.8) %>%
-    addLegend(pal = pal, values = values(x),
-              title = "EDGAR stuffz")
-  # issue: data only from 0-meridian onwards
-  # therefore we lose UK, IE, parts of FR and ES, PT
-  
-  library(raster)
-  library(fasterize)
-  shp_raster <- st_transform(shp, old_crs)
-  shp_raster$random <- runif(nrow(shp_raster))
-  r <- raster(ncol=600, nrow=600)
-  ras <- fasterize(shp_raster, r, field = shp$random, fun = "sum")
-  plot(ras, xlim=c(-30,50),ylim=c(20,80))
+  if(!compareCRS(shape_nuts3, r)) print("Warning: shape and nc file have different CRS!")
   
   
   data_edgar <- data.frame()
@@ -51,12 +32,14 @@ if(! file.exists("./input/data_edgar.rds")) {
   registerDoMC(20)
   data_edgar <- foreach(f = fs) %dopar% {
     
-    b <- brick(f)
-    p <- b$layer 
-    ep <- raster::extract(p, shape_nuts3, method = "simple", fun = mean, na.rm = TRUE, df = TRUE, sp = TRUE)
+    r <- read.delim(file = f, header = T, sep = ";", dec = ".", skip = 2) %>%
+      dplyr::select(lon, lat, value = starts_with("emission")) %>%
+      rasterFromXYZ(crs = "+proj=longlat +datum=WGS84")
     
-    d <- ep@data %>%
-      dplyr::select(nuts3_id, value = layer) %>%
+    er <- raster::extract(r, shape_nuts3, method = "simple", fun = mean, na.rm = TRUE, df = TRUE, sp = TRUE)
+    
+    d <- er@data %>%
+      dplyr::select(nuts3_id, value) %>%
       mutate(year = as.numeric(substr(f, 44, 47)), indicator = c("edgar")) %>%
       dplyr::select(year, nuts3_id, value, indicator)
     
@@ -67,4 +50,3 @@ if(! file.exists("./input/data_edgar.rds")) {
   saveRDS(data_edgar, "input/data_edgar.rds")
   
 }
-
