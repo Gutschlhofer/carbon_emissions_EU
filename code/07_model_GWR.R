aa4b3c53ca6768bdaec6cc0d39c364395b148bec
Jakob Gutschlhofer <h1550545@wigeo0.wu-wien.ac.at>
2021-01-05 17:02:29 +0100

    07_GWR: add GWR with different weighting functions

--- code/07_model_GWR.R
+++ code/07_model_GWR.R
@@ -8,26 +8,56 @@ library(spgwr)
 # gwr.basic
 # gwr.robust
 
-# prepare X and y
-data <- NY8
+source("code/00_libraries_functions.R")
+source("code/06_model_OLS.R")
 
-# write formula
-model <- Z ~ PEXPOSURE + PCTAGE65P + PCTOWNHOME
-model2 <- Cases ~ PEXPOSURE + PCTAGE65P + PCTOWNHOME + offset(log(POP8))
+# data <- st_cast(data, "POLYGON")
+data_coords <- st_coordinates(st_point_on_surface(data$geometry))
+# colnames(data_coords) <- c("Latitude", "Longitud")
+
+# model_base
+# data <- data %>% dplyr::select(edgar, pop, density, gdppc, gwa_share_BE, )
+
+# points from scratch
+# coords = cbind(x, y)
+# sp = SpatialPoints(data_coords)
+# make spatial data frame
+spdf = SpatialPointsDataFrame(data_coords, 
+                              st_drop_geometry(data), 
+                              proj4string = CRS("+proj=longlat +datum=WGS84"))
+
+# NOTE: maybe WGS84 is not ideal?
 
 # find best neighbour specification
 # select bandwidth, here Videras (2014) uses >>gwr.bisquare<<
-bwG <- gwr.sel(model, data = data, gweight = gwr.bisquare, verbose = FALSE)
-gwrG <- gwr(model, data = data, bandwidth = bwG,  gweight = gwr.bisquare, hatmatrix = TRUE)
-gwrG
+bwBisq <- gwr.sel(model_base, data = spdf,# coords = data_coords, 
+                  gweight = gwr.bisquare, longlat = TRUE, verbose = FALSE) #, adapt = T)# verbose = TRUE, 
+bwGauss <- gwr.sel(model_base, data = spdf,# coords = data_coords, 
+                   gweight = gwr.Gauss, longlat = TRUE, verbose = FALSE) #, adapt = T)# verbose = TRUE, 
+bwTri <- gwr.sel(model_base, data = spdf,# coords = data_coords, 
+                 gweight = gwr.tricube, longlat = TRUE, verbose = FALSE) #, adapt = T)# verbose = TRUE, 
 
-# for n_neighbours in 1:n, do
-#     make W for each based on n_neighbours, run GWR
+bwG2 <- gwr.sel(model_base, data = spdf,# coords = data_coords, 
+                gweight = gwr.bisquare, longlat = TRUE, verbose = FALSE, adapt = T)# verbose = TRUE, 
 
-# GWR can also be applied in a GLM
-# framework, and a provisional implementation permitting this has been added to the
-# spgwr package providing both cross-validation bandwidth selection and geographically
-# weighted fitting of GLM models.
-gbwG <- ggwr.sel(model2, data = data, family = "poisson", gweight = gwr.Gauss, verbose = FALSE)
-ggwrG <- ggwr(model2, data = data, family = "poisson", bandwidth = gbwG, gweight = gwr.Gauss)
+# our bw is 969, which basically includes all observations (969/1122)
+# bw <- 285 # or 150 or 450
+bw <- bwGauss
+bw <- 150
+
+gwr <- gwr(model_base, data = data, coords = data_coords, 
+           bandwidth = bw, gweight = gwr.bisquare) #, hatmatrix = TRUE) #, se.fit = TRUE)
+
+gwr
+
+coef <- st_as_sf(gwrG$SDF)
+coef$id <- data$nuts3_id
+st_crs(coef) <- st_crs(data)
+
+data_coef <- cbind(data,st_drop_geometry(coef))
+
+ggplot() + geom_sf(data=data_coef, fill=NA) + geom_sf(data=data_coef, aes(fill=`log.gdppc.`))
+
+# lw = nb2listw()
+gwr.morantest(gwrG1v2, lw = lw)
 
