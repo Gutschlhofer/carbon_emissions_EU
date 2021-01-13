bc91eb47fc1baaf678d46822dffa907a543b43ce
Jakob Gutschlhofer <h1550545@wigeo0.wu-wien.ac.at>
2021-01-13 18:40:29 +0100

    OLS and GWR: change gwa share from log to actual values in our models

--- code/06_model_OLS.R
+++ code/06_model_OLS.R
@@ -12,20 +12,28 @@ data <- readRDS("input/data.rds")
 
 # I=PAT
 model_base <- log(edgar) ~ log(pop) + log(density) + log(gdppc) + 
-  I(log(gdppc)^2) + log(gwa_share_BE) + # log(gwa_share_GI) +
+  I(log(gdppc)^2) + gwa_share_BE + # log(gwa_share_GI) +
   # log(heating_or_cooling) +
   # log(HDD) + cdd_log +
   log(hdd) + log(cdd_fix)
 ols_base <- lm(model_base, data)
 summary(ols_base)
 
+# ols_de <- lm(model_base, data %>% filter(cntr_code == "DE"))
+# ols_de <- lm(model_base, data %>% filter(cntr_code %in% c("DE","PL","CZ")))
+# summary(ols_de)
+
 model_cntr <- log(edgar) ~ log(pop) + log(density) + log(gdppc) + 
-  I(log(gdppc)^2) + log(gwa_share_BE) + # log(gwa_share_GI) +
+  I(log(gdppc)^2) + gwa_share_BE + # log(gwa_share_GI) +
   # log(heating_or_cooling) +
   # log(HDD) + cdd_log +
   log(hdd) + log(cdd_fix) + cntr_code
 ols_cntr <- lm(model_cntr, data)
 summary(ols_cntr)
+
+
+# stargazer(ols_base, ols_cntr, digits=2) # flip = TRUE ? 
+# remove country dummies om cntr
 
 data_coords <- st_coordinates(st_centroid(data$geometry))
 
