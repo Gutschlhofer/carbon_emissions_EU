e1546c1a4491806ff9bcbc7bea98d54e152703f2
Siegrun Gansch <h1107351@wigeo0.wu-wien.ac.at>
2021-01-17 13:08:02 +0100

    renamed shape_nuts1_agg -> shape_nuts0
    auch im 0er und 7er file schon geändert aber dort noch nicht commited

--- code/05_visualise_input.R
+++ code/05_visualise_input.R
@@ -1,6 +1,6 @@
 # load data
 
-# data <- readRDS("input/data.rds")
+data <- readRDS("input/data.rds")
 
 theme_set(theme_minimal())
 
@@ -42,12 +42,12 @@ data$edgar_quantiles <- cut(data$edgar,
 
 
 ggplot(data = data) +
-  geom_sf_pattern(data = shape_nuts1_agg, colour = 'black', fill = 'white') +
+  geom_sf_pattern(data = shape_nuts0, colour = 'black', fill = 'white') +
   geom_sf(aes(fill = edgar_quantiles), color = "white", size=0.01) +
   scale_fill_viridis(direction = -1, discrete = TRUE) + 
   guides(fill = guide_legend(reverse = TRUE)) +
   theme(legend.title = element_blank()) +
-  geom_sf(data=shape_nuts1_agg, color='black', fill=NA, size=0.1) + 
+  geom_sf(data=shape_nuts0, color='black', fill=NA, size=0.1) + 
   theme(plot.margin=grid::unit(c(0,0,0,0), "cm")) 
 ggsave(path = plot_path, filename = sprintf(plot_template, "Edgar_quantiles"), scale=1)
 
@@ -79,35 +79,35 @@ data$gdppc_quantiles <- cut(data$gdppc,
                             include.lowest = T)
 
 ggplot(data = data) +
-  geom_sf_pattern(data = shape_nuts1_agg, colour = 'black', fill = 'white') +
+  geom_sf_pattern(data = shape_nuts0, colour = 'black', fill = 'white') +
   geom_sf(aes(fill = gdppc_quantiles), color = "white", size=0.01) +
   scale_fill_viridis(direction = -1, discrete = TRUE) + 
   guides(fill = guide_legend(reverse = TRUE)) +
   theme(legend.title = element_blank()) +
-  geom_sf(data=shape_nuts1_agg, color='#000000', fill=NA, size=0.1) + 
+  geom_sf(data=shape_nuts0, color='#000000', fill=NA, size=0.1) + 
   theme(plot.margin=grid::unit(c(0,0,0,0), "cm")) 
 ggsave(path = plot_path, filename = sprintf(plot_template, "GDPpc_quantiles"))
 
 
 ## employment shares -----------------------------------------------------------
 ggplot(data = data) + 
-  geom_sf_pattern(data = shape_nuts1_agg, colour = 'black', fill = 'white') +
+  geom_sf_pattern(data = shape_nuts0, colour = 'black', fill = 'white') +
   geom_sf(aes(fill = gwa_share_BE), color = "white", size=0.01) +
   scale_fill_viridis_c(option = "magma", direction = -1, labels = percent) +  
   theme(legend.title = element_blank()) +
-  geom_sf(data=shape_nuts1_agg, color='#000000', fill=NA, size=0.1) + 
+  geom_sf(data=shape_nuts0, color='#000000', fill=NA, size=0.1) + 
   theme(plot.margin=grid::unit(c(0,0,0,0), "cm")) 
 ggsave(path = plot_path, filename = sprintf(plot_template, "GWA_share"))
 
 
 ## CDD -------------------------------------------------------------------------
 cdd <- ggplot(data = data) + 
-  geom_sf_pattern(data = shape_nuts1_agg, colour = 'black', fill = 'white') +
-  #geom_sf(data = shape_nuts1_agg, fill="white") +
+  geom_sf_pattern(data = shape_nuts0, colour = 'black', fill = 'white') +
+  #geom_sf(data = shape_nuts0, fill="white") +
   geom_sf(aes(fill = cdd), color = "white", size=0.01) +
   scale_fill_viridis_c(option = "magma", direction = -1) +
   theme(legend.title = element_blank()) +
-  geom_sf(data=shape_nuts1_agg, color='#000000', fill=NA, size=0.1) + 
+  geom_sf(data=shape_nuts0, color='#000000', fill=NA, size=0.1) + 
   theme(plot.margin=grid::unit(c(0,0,0,0), "cm")) 
 cdd
 ggsave(path = plot_path, filename = sprintf(plot_template, "CDD")) 
@@ -115,11 +115,11 @@ ggsave(path = plot_path, filename = sprintf(plot_template, "CDD"))
 
 ## HDD -------------------------------------------------------------------------
 hdd <- ggplot(data = data) + 
-  geom_sf_pattern(data = shape_nuts1_agg, colour = 'black', fill = 'white') + 
+  geom_sf_pattern(data = shape_nuts0, colour = 'black', fill = 'white') + 
   geom_sf(aes(fill = hdd), color = "white", size=0.01) +
   scale_fill_viridis_c(direction = -1) +
   theme(legend.title = element_blank()) + 
-  geom_sf(data=shape_nuts1_agg, color='#000000', fill=NA, size=0.1) + 
+  geom_sf(data=shape_nuts0, color='#000000', fill=NA, size=0.1) + 
   theme(plot.margin=grid::unit(c(0,0,0,0), "cm")) 
   #theme(plot.background = element_rect(fill = NA))
 hdd
@@ -136,13 +136,13 @@ ggsave(path = plot_path, filename = sprintf(plot_template, "CDD_HDD"))
 ## density ---------------------------------------------------------------------
 
 # ggplot(data = data) +
-#   geom_sf(data = shape_nuts1_agg, color="grey") +
+#   geom_sf(data = shape_nuts0, color="grey") +
 #   geom_sf(aes(fill = density), color = "black", size=0.1) +
 #   ggtitle ("Population density")  +
 #   scale_fill_viridis(direction = -1) +
 #   guides(fill = guide_legend(reverse = TRUE)) +
 #   theme(legend.title = element_blank()) +
-#   geom_sf(data=shape_nuts1_agg, color='#000000', fill=NA, size=0.1)
+#   geom_sf(data=shape_nuts0, color='#000000', fill=NA, size=0.1)
 
 # no_classes <- 5
 
@@ -166,12 +166,12 @@ data$density_quantiles <- cut(data$density,
                             include.lowest = T)
 
 ggplot(data = data) +
-  geom_sf_pattern(data = shape_nuts1_agg, colour = 'black', fill = 'white') + 
+  geom_sf_pattern(data = shape_nuts0, colour = 'black', fill = 'white') + 
   geom_sf(aes(fill = density_quantiles), color = "white", size=0.01) +
   scale_fill_manual(values = rev(magma(8)[3:8])) +
   guides(fill = guide_legend(reverse = TRUE)) +
   theme(legend.title = element_blank()) +
-  geom_sf(data=shape_nuts1_agg, color='#000000', fill=NA, size=0.1) + 
+  geom_sf(data=shape_nuts0, color='#000000', fill=NA, size=0.1) + 
   # theme(plot.margin=grid::unit(c(0,0,0,0), "cm")) +
   # theme(plot.background = element_rect(fill = NA) )
 theme(plot.margin = unit(c(1,1,1,1), "cm"))
