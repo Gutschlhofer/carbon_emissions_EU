b6a253a191ece38aa1482463f3af8817a8c5fb5c
Benjamin Gerhard Müller <h1551800@wigeo0.wu-wien.ac.at>
2021-01-24 14:26:48 +0100

    Clean-up and comments 00_libraries_functions.R

--- code/00_libraries_functions.R
+++ code/00_libraries_functions.R
@@ -1,6 +1,6 @@
 # Load libraries
 # install.packages("pacman")
-library(pacman)
+library(pacman) # download and load, load only if already downloaded
 
 p_load(tidyverse)
 p_load(eurostat)
@@ -26,7 +26,7 @@ p_load(cowplot)
 # add new packages with p_load(packagename)
 
 
-## eurostat NUTS 3 shapefile----------------------------------------------------
+## Eurostat NUTS 3 shapefile----------------------------------------------------
 
 # this is the shapefile for 2021, we use the eurostat package which has the year 2016
 getShapefile <- function(replace = FALSE){
@@ -42,7 +42,7 @@ getShapefile <- function(replace = FALSE){
     # create a directory
     dir.create("input/shapefile")
     
-    shape_nuts3 <- eurostat::get_eurostat_geospatial(output_class="sf", resolution="1", nuts_level=3, year=2016) %>% 
+    shape_nuts3 <- eurostat::get_eurostat_geospatial(output_class="sf", resolution="60", nuts_level=3, year=2016) %>% 
       dplyr::rename("nuts3_id" = "NUTS_ID")
     names(shape_nuts3) <- tolower(names(shape_nuts3))
     # plot(sf::st_geometry(shape_nuts3))
@@ -73,25 +73,8 @@ getShapefile <- function(replace = FALSE){
     shape_equalarea <- st_drop_geometry(shape_equalarea)
     
     shape_nuts3 <- left_join(shape_nuts3, shape_equalarea)
-    
-    # # download the zip file with all zip files of the shapefiles
-    # download.file("https://gisco-services.ec.europa.eu/distribution/v2/nuts/download/ref-nuts-2021-01m.shp.zip",
-    #               "input/shapefile/NUTS_2021_01M.zip")
-    # 
-    # # unzip the zip file with all the zipped shapefiles
-    # unzip("input/shapefile/NUTS_2021_01M.zip", files = "NUTS_RG_01M_2021_4326_LEVL_3.shp.zip",
-    #       exdir = "./input/shapefile")
-    # 
-    # # unzip the shapefile of interest with the following specifications
-    # # RG = regions (multipolygons)
-    # # 4326 = EPSG:4326 (WGS84, coordinates in decimal degrees)
-    # # LEVL_3 = NUTS level 3
-    # unzip("input/shapefile/NUTS_RG_01M_2021_4326_LEVL_3.shp.zip", exdir = "input/shapefile")
-    # 
-    # # load the shapefile and remove the zip file with all the zipped shapefiles
-    # shape_nuts3 <- st_read("./input/shapefile", "NUTS_RG_01M_2021_4326_LEVL_3")
-    # file.remove("input/shapefile/NUTS_2021_01M.zip")
-  
+
+    # save shapefile with area
     unlink("./input/shapefile/nuts3.shp")
     sf::st_write(shape_nuts3, "./input/shapefile/nuts3.shp")
     
@@ -100,21 +83,22 @@ getShapefile <- function(replace = FALSE){
 }
 
 
-# we need a nuts0 shapefile so that we can add country borders to the plot
-# to exclude regions overseas, exclude them from nuts1 level 
-shape_nuts1 <- eurostat::get_eurostat_geospatial(output_class="sf", resolution="1", nuts_level=1, year=2016) 
+# we need a NUTS0 shapefile so that we can add country borders to the plot
+# to exclude regions overseas, exclude them from NUTS1 level 
+shape_nuts1 <- eurostat::get_eurostat_geospatial(output_class="sf", resolution="60", nuts_level=1, year=2016) 
 names(shape_nuts1) <- tolower(names(shape_nuts1))
 
-# then aggregate based on ctnr_code (and exclude turkey, iceland and cyprus for nicer and more centred plots)
+# then aggregate based on ctnr_code (and exclude Turkey, Iceland and Cyprus for nicer and more centered plots)
 shape_nuts0 <- shape_nuts1 %>% 
-  filter(!nuts_id %in% c("FRY", "PT2", "PT3", "ES7") & (cntr_code != "TR" & cntr_code != "IS" & cntr_code != "CY") ) %>% 
+  dplyr::filter(!nuts_id %in% c("FRY", "PT2", "PT3", "ES7") & (cntr_code != "TR" & cntr_code != "IS" & cntr_code != "CY") ) %>% 
   count(cntr_code)
 
 # this is needed for st_pattern 
 shape_nuts0 <- st_remove_holes(shape_nuts0)
 
 
-# add kosovo and bosnia to the shapefile
+# add Kosovo and Bosnia to the shapefile for NA line pattern
+# downloaded here: https://gadm.org/index.html
 ba <- readRDS("./input/shapefile_ba_xk/gadm36_BIH_0_sf.rds") %>%
   rename(cntr_code = "GID_0",
          n = "NAME_0",
@@ -129,9 +113,8 @@ xk <- readRDS("./input/shapefile_ba_xk/gadm36_XKO_0_sf.rds") %>%
 xk$cntr_code <- "XK"
 xk$n <- 1
 
-shape_nuts0 <- rbind(shape_nuts0, ba)
-shape_nuts0 <- rbind(shape_nuts0, xk)
+# rbind to shapefile
+shape_nuts0 <- rbind(shape_nuts0, ba, xk)
 
+# buffer one last time if something went wrong
 shape_nuts0 <- st_buffer(shape_nuts0, dist = 0)
-
-
