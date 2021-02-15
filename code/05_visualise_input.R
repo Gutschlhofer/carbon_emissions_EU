338637ffcf3e11f156c8b616092644aec8b4ed38
Benjamin Gerhard Müller <h1551800@wigeo0.wu-wien.ac.at>
2021-02-15 11:13:01 +0100

    Added quantile function and changed some plot settings

--- code/05_visualise_input.R
+++ code/05_visualise_input.R
@@ -1,5 +1,5 @@
 # load data
-data <- readRDS("input/data.rds")
+# data <- readRDS("input/data.rds")
 
 # ensure that all plots have the same theme
 theme_set(theme_minimal())
@@ -9,43 +9,46 @@ plot_path <- "output/plots/"
 plot_template <- paste0("raw_%s",".png")
 
 # save plots?
-s <- F
+s <- T
 
-
-
-## EDGAR data ------------------------------------------------------------------
 # divide into 5 quantiles + corresponding legend
-no_classes <- 5
-quantiles <- quantile(data$edgar, probs = seq(0, 1, length.out = no_classes + 1))
-
-labels <- c()
-for(idx in 1:length(quantiles)){
-  labels <- c(labels, paste0(round(quantiles[idx], -2),
-                             " – ",
-                             round(quantiles[idx + 1], -2)))
+plot_quantiles <- function(data, variable_name, plot_name, s = F) {
+  no_classes <- 5
+  var_vector <- data %>% pull(all_of(variable_name))
+  quantiles <- quantile(var_vector, probs = seq(0, 1, length.out = no_classes + 1))
+  
+  labels <- c()
+  for(idx in 1:length(quantiles)){
+    labels <- c(labels, paste0(round(quantiles[idx], -2),
+                               " – ",
+                               round(quantiles[idx + 1], -2)))
+  }
+  
+  # remove the last label because that would be something like "100.000 - NA"
+  labels <- labels[1:length(labels)-1]
+  
+  data$quantiles <- cut(var_vector, 
+                        breaks = quantiles, 
+                        labels = labels, 
+                        include.lowest = T)
+  
+  p <- ggplot(data = data) +
+    geom_sf_pattern(data = shape_nuts0, colour = 'black', fill = 'white', pattern = 'stripe',                    
+                    pattern_size = 0.5, pattern_linetype = 1, pattern_spacing = 0.008,                    
+                    pattern_fill = "white", pattern_density = 0.1, pattern_alpha = 0.7) + 
+    geom_sf(aes(fill = quantiles), color = "white", size=0.01) +
+    scale_fill_viridis(direction = -1, discrete = TRUE) + 
+    guides(fill = guide_legend(reverse = TRUE)) +
+    theme(legend.title = element_blank()) +
+    geom_sf(data=shape_nuts0, color='black', fill=NA, size=0.1) + 
+    theme(plot.margin=grid::unit(c(0,0,0,0), "cm")) 
+  if(s) ggsave(p, path = plot_path, filename = sprintf(plot_template, plot_name), scale=1, width = 5, height = 5)
+  
+  return(p)
 }
 
-# remove the last label because that would be something like "100.000 - NA"
-labels <- labels[1:length(labels)-1]
-
-data$edgar_quantiles <- cut(data$edgar, 
-                            breaks = quantiles, 
-                            labels = labels, 
-                            include.lowest = T)
-
-ggplot(data = data) +
-  geom_sf_pattern(data = shape_nuts0, colour = 'black', fill = 'white', pattern = 'stripe',                    
-                  pattern_size = 0.5, pattern_linetype = 1, pattern_spacing = 0.008,                    
-                  pattern_fill = "white", pattern_density = 0.1, pattern_alpha = 0.7) + 
-  geom_sf(aes(fill = edgar_quantiles), color = "white", size=0.01) +
-  scale_fill_viridis(direction = -1, discrete = TRUE) + 
-  guides(fill = guide_legend(reverse = TRUE)) +
-  theme(legend.title = element_blank()) +
-  geom_sf(data=shape_nuts0, color='black', fill=NA, size=0.1) + 
-  theme(plot.margin=grid::unit(c(0,0,0,0), "cm")) 
-if(s) ggsave(path = plot_path, filename = sprintf(plot_template, "Edgar_quantiles"), scale=1)
-
-
+## EDGAR data ------------------------------------------------------------------
+plot_quantiles(data, variable_name = "edgar", plot_name = "Edgar_quantiles", s = s)
 
 ## GDP p.c. --------------------------------------------------------------------
 no_classes <- 5
@@ -75,7 +78,7 @@ gdp <- ggplot(data = data) +
   theme(legend.title = element_blank()) +
   geom_sf(data=shape_nuts0, color='#000000', fill=NA, size=0.1) + 
   theme(plot.margin=grid::unit(c(0,0,0,0), "cm")) 
-if(s) ggsave(gdp ,path = plot_path, filename = sprintf(plot_template, "GDPpc_quantiles"))
+if(s) ggsave(gdp ,path = plot_path, filename = sprintf(plot_template, "GDPpc_quantiles"), width = 5, height = 5)
 
 
 
@@ -89,7 +92,7 @@ gwa <- ggplot(data = data) +
   theme(legend.title = element_blank()) +
   geom_sf(data=shape_nuts0, color='#000000', fill=NA, size=0.1) + 
   theme(plot.margin=grid::unit(c(0,0,0,0), "cm"))
-if(s) ggsave(gwa, path = plot_path, filename = sprintf(plot_template, "GWA_share"))
+if(s) ggsave(gwa, path = plot_path, filename = sprintf(plot_template, "GWA_share"), width = 4, height = 5)
 
 # possibly only relevant for presentation: combined GDP & GWA share 
 # GDP_GWA <- plot_grid(gdp, gwa, ncol = 2)
@@ -107,7 +110,7 @@ cdd <- ggplot(data = data) +
   theme(legend.title = element_blank()) +
   geom_sf(data=shape_nuts0, color='#000000', fill=NA, size=0.1) + 
   theme(plot.margin=grid::unit(c(0,0,0,0), "cm"))
-if(s) ggsave(cdd, path = plot_path, filename = sprintf(plot_template, "CDD")) 
+if(s) ggsave(cdd, path = plot_path, filename = sprintf(plot_template, "CDD"), width = 4, height = 5) 
 
 
 
@@ -121,16 +124,16 @@ hdd <- ggplot(data = data) +
   theme(legend.title = element_blank()) + 
   geom_sf(data=shape_nuts0, color='#000000', fill=NA, size=0.1) + 
   theme(plot.margin=grid::unit(c(0,0,0,0), "cm"))
-if(s) ggsave(hdd, path = plot_path, filename = sprintf(plot_template, "HDD")) 
+if(s) ggsave(hdd, path = plot_path, filename = sprintf(plot_template, "HDD"), width = 4, height = 5) 
 
 # combined HDD and CDD
 HDD_CDD <- plot_grid(hdd, cdd, ncol = 2)
-if(s) ggsave(plot = HDD_CDD, path = plot_path, filename = sprintf(plot_template, "HDD_CDD")) 
+if(s) ggsave(plot = HDD_CDD, path = plot_path, filename = sprintf(plot_template, "HDD_CDD"), width = 10, height = 6) 
 
 
 
 ## population ------------------------------------------------------------------
-data$pop <- data$pop/1000
+data$pop <- data$pop/1000 # in 1000 people
 
 no_classes <- 5
 quantiles_pop <- quantile(data$pop, 
@@ -166,7 +169,6 @@ data$pop <- data$pop*1000
 
 
 ## density ---------------------------------------------------------------------
-data$density <- data$density*(1000)
 
 no_classes <- 5
 quantiles_density <- quantile(data$density, 
@@ -174,10 +176,10 @@ quantiles_density <- quantile(data$density,
 
 labels <- c()
 for(idx in 1:length(quantiles_density)){
-  labels <- c(labels, paste0(round(quantiles_density[idx], -1),
-                             "k – ",
-                             paste0(round(quantiles_density[idx + 1], -1), 
-                                    "k")))
+  labels <- c(labels, paste0(round(quantiles_density[idx], 3),
+                             " – ",
+                             paste0(round(quantiles_density[idx + 1], 3)
+                                    )))
 }
 
 labels <- labels[1:length(labels)-1]
@@ -197,14 +199,12 @@ dens <- ggplot(data = data) +
   theme(legend.title = element_blank()) +
   geom_sf(data=shape_nuts0, color='#000000', fill=NA, size=0.1) + 
   theme(plot.margin=grid::unit(c(0,0,0,0), "cm"))
-if(s) ggsave(dens, path = plot_path, filename = sprintf(plot_template, "Density_quantiles"))
+if(s) ggsave(dens, path = plot_path, filename = sprintf(plot_template, "Density_quantiles"), width = 4, height = 5)
 
-data$density <- data$density/(1000/1000^2)
 
 # pop_dens
 pop_dens <- plot_grid(pop, dens, ncol = 2)
-if(s) ggsave(pop_dens, path = plot_path, filename = sprintf(plot_template, "Pop_Dens"))
-
+if(s) ggsave(pop_dens, path = plot_path, filename = sprintf(plot_template, "Pop_Dens"), width = 10, height = 6)
 
 
 # Summary table ----------------------------------------------------------------
@@ -225,14 +225,14 @@ sum_data <- sum_data %>% dplyr::select(gdppc, density, gwa_share_BE, edgar, hdd,
 # equivalent to table 1 from videras
 sum_data_log <- log(sum_data)
 
-summary2 <- cbind(as.data.frame(descr(sum_data,
+summary2 <- rbind(as.data.frame(descr(sum_data,
                                 stats = c("mean", "sd"),
                                 order = "p")),
                     as.data.frame(descr(sum_data_log,
                                 stats = c("mean", "sd"),
                                 order = "p")))
 
-rownames(summary2) <- c("GDP p.c.", "Population density", "Empl. share in manufact.", "CO² emission levels", "Heating Days Index", "Cooling Days Index")
-colnames(summary2) <- c("Mean (orig. values)", "Std.Dev (orig. values)", "Mean (log values)", "Std.Dev (log values)")
+colnames(summary2) <- c("GDP p.c.", "Population density", "Empl. share in manufact.", "CO² emission levels", "HDD", "CDD")
+rownames(summary2) <- c("Mean (orig. values)", "Std.Dev (orig. values)", "Mean (log values)", "Std.Dev (log values)")
 summary2 <- round(summary2, 2)
 summary2
