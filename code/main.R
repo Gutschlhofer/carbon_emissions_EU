aad0a36fd5cb6de19783c72b6fe5f2685d5c0797
Georg Leitner <h1622257@wigeo0.wu-wien.ac.at>
2021-02-15 16:35:22 +0100

    main.R cleanup

--- code/main.R
+++ code/main.R
@@ -32,19 +32,3 @@ source("code/06_model_OLS.R")
 
 # Prepare and run GWR ----------------------------------------------------------
 source("code/07_model_GWR.R")
-
-
-
-# RMarkdown stuff  -------------------------------------------------------------
-# data <- readRDS("input/data.rds")
-# data_coef <- readRDS("input/data_coef.rds")
-# gwr <- readRDS("input/gwr.rds")
-# 
-# # knit the short presentation  -------------------------------------------------
-# file_name <- "./code/P1_short_presentation"
-# rmarkdown::render(paste0(file_name,".Rmd"))
-# 
-# # knit the final presentation  -------------------------------------------------
-# file_name <- "./code/P2_final_presentation"
-# rmarkdown::render(paste0(file_name,".Rmd"))
-
